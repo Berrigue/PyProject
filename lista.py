371ff263690c522b2f9e811f7e1a8bd35c31ea5b
Alexandre Berrigue <berrigue@gmail.com>
2024-11-18 21:49:14 -0300

    Adicionando lista

--- lista.py
+++ lista.py
@@ -5,4 +5,12 @@ n2 = [10,20,30,40,50,60,70,80]
 
 listaFechada = n1 + n2
 
-print(listaFechada)
+
+#acessar apenas os 4 primeiros numeros da lista
+print(listaFechada[:4])
+
+#altera um valor da lista em sua respectiva posicao
+listaFechada[-1] = 81
+
+#print(listaFechada)
+print(listaFechada[-1])
